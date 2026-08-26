686898a89e83fe3d5ea66dcb8b52e2c77ca7bd63
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-08-26 11:48:56 +0700

    auto 2026-08-26 11:48:56

--- file3.py
+++ file3.py
@@ -10,6 +10,14 @@ clock = pygame.time.Clock()
 bg_surface = pygame.image.load("background.jpg")
 bg_surface = pygame.transform.scale(bg_surface, (WIDTH, HEIGHT))
 
+ship_width, ship_height = 150,100
+ship_surface = pygame.image.load('ship3.png').convert_alpha()
+ship_surface = pygame.transform.scale(ship_surface, (ship_width, ship_height))
+ship_x = WIDTH//2
+ship_y = HEIGHT
+ship_rect = ship_surface.get_rect(midbottom=(ship_x, ship_y))
+ship_speed = 20
+
 
 running = True
 while running:
@@ -17,8 +25,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        ship_rect.x -= ship_speed
+    if keys[pygame.K_RIGHT]:
+        ship_rect.x += ship_speed
+    if ship_rect.x < 0:
+        ship_rect.x = 0
+    if ship_rect.x > WIDTH-ship_width:
+        ship_rect.x = WIDTH-ship_width
+        
+
 
     screen.blit(bg_surface, (0,0))
+    screen.blit(ship_surface, ship_rect)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
