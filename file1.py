bda7a2b6a0b098cbfc1a31c2cd592d5b3ef9434b
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-08-26 14:43:57 +0700

    auto 2026-08-26 14:43:57

--- file1.py
+++ file1.py
@@ -1,1 +1,13 @@
 import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5,6,7,8]
+y = [45,50,55,62,68,72,78,85]
+
+plt.plot(x,y, marker='o', label ="คะแนนสอบ")
+
+plt.xlabel("ชั่วโมงอ่านหนังสือสอบ")
+plt.ylabel("คะแนนสอบ")
+plt.title("ความสัมพันธ์ระหว่างชั่วโมงอ่านหนังสือสอบกับคะแนนสอบ")
+
+plt.legend()
+plt.show()
