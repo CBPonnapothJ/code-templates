dc90b72f93ae0d174050ee5f5ed085ffc3f7cac2
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-09-13 13:43:12 +0700

    auto 2026-09-13 13:43:12

---
+++ file1.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+pygame.init()
+
+WIDTH, HEIGHT = 800 , 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space shooter")
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load("background.jpg").convert_alpha()
+bg_surface = pygame.transform.scale(bg_surface, (WIDTH, HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(bg_surface, (0,0))
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
