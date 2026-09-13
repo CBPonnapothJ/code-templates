443c1d0892207eebe3fd7653e5dfdc237dd7b040
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-09-13 15:12:20 +0700

    auto 2026-09-13 15:12:20

--- file2.py
+++ file2.py
@@ -45,21 +45,43 @@ while running:
         meteors.append(meteor_rect)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        rocket_rect.centerx -= 7
-    if keys[pygame.K_RIGHT]:
-        rocket_rect.centerx += 7
-    if rocket_rect.left < 0:
-        rocket_rect.left = 0
-    if rocket_rect.right > WIDTH:
-        rocket_rect.right = WIDTH
+    if not game_over:
+        if keys[pygame.K_LEFT]:
+            rocket_rect.centerx -= 7
+        if keys[pygame.K_RIGHT]:
+            rocket_rect.centerx += 7
+        if rocket_rect.left < 0:
+            rocket_rect.left = 0
+        if rocket_rect.right > WIDTH:
+            rocket_rect.right = WIDTH
+
+    for bullet in bullets:
+        bullet.y -= 10
+        if bullet.y < 0:
+            bullets.remove(bullet)
+
+    for meteor in meteors[:]:
+        meteor.y += 4
+        if meteor.y > HEIGHT:
+            meteors.remove(meteor)
+
+        if rocket_rect.colliderect(meteor):
+            game_over = True
+
+        for bullet in bullets[:]:
+            if bullet.colliderect(meteor):
+                bullets.remove(bullet)
+                meteors.remove(meteor)
+                score += 1
+                break
+
 
     screen.blit(bg, (0, 0))
     screen.blit(rocket_image, rocket_rect)
-
+    for meteor in meteors[:]:
+        screen.blit(meteor_image, meteor)
+    for bullet in bullets[:]:
+        pygame.draw.rect(screen,(255,255,0), bullet)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
-
-
-
