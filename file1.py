2ea95debd6df8d91084d15ba7e68506156d17d10
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-09-13 14:00:28 +0700

    auto 2026-09-13 14:00:28

--- file1.py
+++ file1.py
@@ -37,7 +37,7 @@ while running:
     screen.blit(bg, (0,0))
     screen.blit(rocket_image, rocket_rect)
     for meteor in meteor_positions:
-        screen.blit(meteor_images[meteor[0]], 50)
+        screen.blit(meteor_images[meteor[0]], meteor[1])
 
 
     pygame.display.flip()
