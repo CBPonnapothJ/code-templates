b5a3cf49b41275d70912c1941d7b1cc25968a4bf
CBPonnapothJ <ponnapoth.j@cubiccreative.org>
2026-09-13 15:35:18 +0700

    auto 2026-09-13 15:35:18

--- file2.py
+++ file2.py
@@ -22,7 +22,7 @@ meteor_image = pygame.transform.scale(meteor_image, (40, 40))
 shooting_sound = pygame.mixer.Sound("shot-sound.wav")
 hit_sound = pygame.mixer.Sound("explosion-sound.wav")
 bgm_sound = pygame.mixer.music.load("BGM2.mp3")
-bgm_sound.
+pygame.mixer.music.play(-1)
 
 bullets = []
 meteors = []
